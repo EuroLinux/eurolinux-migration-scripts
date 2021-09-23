4dc47260fe235d7078c6f9a026a4471a3e98adef
Tomasz Podsiadły <tp@euro-linux.com>
2021-09-23 13:59:45 +0200

    el_distro_sync

--- migrate2eurolinux.sh
+++ migrate2eurolinux.sh
@@ -573,6 +573,7 @@ update_initrd() {
 }
 
 el_distro_sync() {
+  # Make sure all packages are synchronized with the ones EuroLinux provides.
   echo "Switch successful. Syncing with EuroLinux repositories..."
   if ! yum -y distro-sync; then
     exit_message "Could not automatically sync with EuroLinux repositories.
