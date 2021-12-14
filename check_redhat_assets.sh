8d58d79a4859406b2bd49988e4030bf572794da2
Tomasz Podsiadły <tp@euro-linux.com>
2021-12-14 10:16:36 +0100

    New pattern for determining Red Hat assets

--- check_redhat_assets.sh
+++ check_redhat_assets.sh
@@ -42,8 +42,10 @@ before_migration() {
     | xargs rpm -ql --noconfig > all_redhat_assets.txt
 
   # Red Hat-branded images and text assets
-  grep -E '^redhat|^Red' "all_redhat_packages.txt" | cut -d'@' -f1 \
-    | xargs rpm -ql --noconfig | grep -Ei 'fedora.logo|red' \
+  grep -E 'insights|redhat|rhel|Red' "all_redhat_packages.txt" \
+    | cut -d'@' -f1 \
+    | xargs rpm -ql --noconfig \
+    | grep -Ei 'fedora.logo|redhat|Red|rhel' \
     | while IFS= read -r file; do \
         [ -f "$file" ] && printf '%s\n' "$file" ; \
       done > assets_to_check.txt
@@ -56,8 +58,8 @@ after_migration() {
     echo "Run ${0##*/} -b (before migrating to EuroLinux) first."
   else
     if [ ! -s "all_redhat_packages.txt" ] ; then
-      echo "It's pointless to perform any checks if the previous system did not
-      contain any Red Hat packages, exiting."
+      echo "It's pointless to perform any checks if the previous
+      system did not contain any Red Hat packages, exiting."
       exit 0
     fi
   fi
