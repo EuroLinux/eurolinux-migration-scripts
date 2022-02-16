9e4bc2e3b4bd48db46edc6811e796376f3e35ede
Tomasz Podsiadły <tp@euro-linux.com>
2022-02-16 09:52:36 +0100

    Move the getopts code snippet earlier in the post-migration script

--- test_what_non_el_remains_after_migration.sh
+++ test_what_non_el_remains_after_migration.sh
@@ -10,6 +10,13 @@ usage() {
 
 tolerance=""
 
+while getopts "t" option; do
+    case "$option" in
+        t) tolerance="tolerate-thirdparty" ;;
+        *) usage ;;
+    esac
+done
+
 echo "Performing post-migration checks..."
 mapfile -t non_eurolinux_rpms_from_yum_list < <(yum list installed | sed '/^[^@]*$/{N;s/\n//}' | grep -Ev '@el-server-|@euroman|@fbi|@certify' | grep '@' | cut -d' ' -f 1 | cut -d'.' -f 1)
   mapfile -t non_eurolinux_rpms_and_metadata < <(rpm -qa --qf "%{NEVRA}|%{VENDOR}|%{PACKAGER}\n" ${non_eurolinux_rpms_from_yum_list[*]} | grep -Ev 'EuroLinux|Scientific' | sed 's@\ @\_@g' | grep -Ev '^(rhnlib|rhnsd).+\|\(none\)\|\(none\)$') 
@@ -32,11 +39,4 @@ if [[ -n "${non_eurolinux_rpms_and_metadata[*]}" ]]; then
 fi
 echo "Success - no non-EuroLinux RPMs are installed on the system."
 exit 0
-
-while getopts "t" option; do
-    case "$option" in
-        t) tolerance="tolerate-thirdparty" ;;
-        *) usage ;;
-    esac
-done
 
