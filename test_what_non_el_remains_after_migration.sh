11722ae23381352e6be8a9b98b1ded4d49bea94f
Kamil Aronowski <ka@euro-linux.com>
2022-08-08 16:23:10 +0200

    Reformat

--- test_what_non_el_remains_after_migration.sh
+++ test_what_non_el_remains_after_migration.sh
@@ -23,18 +23,24 @@ mapfile -t non_eurolinux_rpms_from_yum_list < <(yum list installed | sed '/^[^@]
 if [[ -n "${non_eurolinux_rpms_and_metadata[*]}" ]]; then
   echo "The following non-EuroLinux RPMs are installed on the system:"
   printf '\t%s\n' "${non_eurolinux_rpms_and_metadata[@]}"
-  if [ "$tolerance" == "tolerate-thirdparty" ]; then # Tolerate packages e.g. from EPEL, do not tolerate from AlmaLinux/CentOS/Oracle Linux/RHEL/Rocky Linux.
-    echo "Checking for the presence of packages that come from the migratable systems..."
+  if [ "$tolerance" == "tolerate-thirdparty" ]; then
+  # Tolerate packages e.g. from EPEL, do not tolerate from
+  # AlmaLinux/CentOS/Oracle Linux/RHEL/Rocky Linux.
+    echo "Checking for the presence of packages that come from the
+migratable systems..."
     bad_providers_pattern="\|AlmaLinux\||\|CentOS\||\|Oracle_America\||\|Red_Hat,_Inc\.\||\|Rocky\|"
     if grep -E "$bad_providers_pattern" <<< "${non_eurolinux_rpms_and_metadata[@]}" ; then
-      echo "^ the packages above still remain - the migration is not considered as 100% complete. Please remove them manually."
+      echo "^ the packages above still remain - the migration is
+not considered as 100% complete. Please remove them manually."
       exit 1
     else
-      echo "(none, the migration removed all packages from migratable systems)"
+      echo "(none, the migration removed all packages from
+migratable systems)"
       exit 0
     fi
   else
-    echo "Since the test requires that no thirdparty packages be present on the system, we consider this result as a failure."
+    echo "Since the test requires that no thirdparty packages be
+present on the system, we consider this result as a failure."
     exit 1
   fi
 fi
